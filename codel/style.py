9a9588201713efed71ca4dbb3dd66367b871694e
VoIlAlex <ilya.vouk@gmail.com>
2020-04-06 18:28:30 +0300

    Add _FolderNameStylizerBlock

--- codel/style.py
+++ codel/style.py
@@ -16,3 +16,15 @@ class _Stylizer(_CollectorApplicable):
         for block in self.stylizer_blocks:
             result += block.apply(collector) + '\n'
         return result
+
+
+class _FolderNameStylizerBlock(_CollectorApplicable):
+    def apply(self, collector: FilesCollector) -> str:
+        return '{}{}Directory:{} {}'.format(
+            fg(149),
+            attr(1),
+            attr(0),
+            collector.folder_name
+        )
+
+
